37877c6d8d2acaf9bddb74cc5ff00fe0eea0377e
Jacek <69013954+Jacux@users.noreply.github.com>
2023-10-01 15:04:18 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,7 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-RegisterNetEvent("carism_narcoTaxi:pay")
-AddEventHandler('carism_narcoTaxi:pay', function()
+RegisterNetEvent("Jacux_NarcoTaxi:pay")
+AddEventHandler('Jacux_NarcoTaxi:pay', function()
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     xPlayer.addMoney(math.random(300, 500))
